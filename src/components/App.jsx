ccc47200fe56554179853e66dce16af28b30252f
marvinchin <chinkunsong.marvin@gmail.com>
2017-05-21 19:10:56 +0800

    Set up base react application

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Welcome to Stork!
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
