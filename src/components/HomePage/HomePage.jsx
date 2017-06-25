3bcd12b8785c0cd2ba1e01f538e7e2a383df3573
marvinchin <chinkunsong.marvin@gmail.com>
2017-06-25 19:27:51 +0800

    Added functionality to get most recent books on home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import SearchBar from '../SearchBar';
 import BookCollection from '../BookCollection';
+import { getIndexBooks } from '../../ActionCreators/BookActionCreators';
 
 class HomePage extends Component {
+  componentDidMount() {
+    this.props.dispatch(getIndexBooks());
+  }
+
   render() {
+    const { indexBooks } = this.props;
     return (
       <div className="c-home">
         <SearchBar />
-        <BookCollection />
+        <BookCollection books={indexBooks} />
       </div>
     );
   }
 }
 
-export default HomePage;
+HomePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  indexBooks: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = state => (
+  {
+    indexBooks: state.books.indexBooks,
+  }
+);
+
+export default connect(mapStateToProps)(HomePage);
