69f9cf21f3000a998a087cc78139818f40d564c9
marvinchin <chinkunsong.marvin@gmail.com>
2017-07-21 23:12:23 +0800

    Update config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export default {
-  // BACKEND_API_URL: "http://api.storkapp.flu.cc",
-  BACKEND_API_URL: "http://45.55.191.88:3000",
+  BACKEND_API_URL: "http://api.storkapp.flu.cc",
   SESSION_EXPIRY: 60 * 60 * 24,
   NUM_BOOKS_PER_LOAD: 30,
   DEFAULT_USER_IMAGE_PATH: "/profile-pictures/_default.jpg"
