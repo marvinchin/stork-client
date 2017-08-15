8cef4dc378172bb5655dceca3841bf688897a9a2
marvinchin <chinkunsong.marvin@gmail.com>
2017-08-15 23:10:53 +0800

    Update config file to point to production

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 export default {
   // BACKEND_API_URL: "http://api.storkapp.flu.cc",
-  BACKEND_API_URL: "http://localhost:3000",
-  SOCKET_URL: "http://localhost:3000",
+  // BACKEND_API_URL: "http://localhost:3000",
+  BACKEND_API_URL: "http://45.55.191.88:3000",
+  SOCKET_URL: "http://45.55.191.88:3000",
+  // SOCKET_URL: "http://localhost:3000",
   SESSION_EXPIRY: 60 * 60 * 24,
   NUM_BOOKS_PER_LOAD: 30,
   DEFAULT_USER_IMAGE_PATH: "/profile-pictures/_default.jpg"
